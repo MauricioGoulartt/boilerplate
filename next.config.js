5d9dc94f6dd7cc9e9816d6ac01e6412060e36135
mauricio <mauriciogoulart.1990@gmail.com>
2023-08-03 19:44:20 -0300

    add PWA config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')({
-  dest: 'public'
-})
+const inProd = process.env.NODE_ENV === 'production'
 
-module.exports = withPWA({
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')
+
+export default withPWA({
   // next.js config
+  pwa: {
+    dest: 'public',
+    disable: !inProd
+  }
 })
